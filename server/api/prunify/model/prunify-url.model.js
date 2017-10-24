68bedc375c238cb30929234d6bc66e1e3065d094
Kaustav Majumder <pupunmajumder@gmail.com>
2017-10-25 01:22:14 +0530

    test

--- server/api/prunify/model/prunify-url.model.js
+++ server/api/prunify/model/prunify-url.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const _urlSchema = {
    _id :{type :Number,index :true},
   long_url : {type:String,required: true},
-   created_at : Date
+   created_at : {type :Date,default:Date.now()}
 };
 
 module.exports = mongoose.Schema(_urlSchema);
